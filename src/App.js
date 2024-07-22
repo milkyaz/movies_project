5d7002cf297acb32045eeef81d2e2667aed10e27
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-07-22 18:55:37 +0400

    Добавил компонет Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import { Header } from "./layout/Header";
+import { Footer } from "./layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
 
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,24 @@
 function Header() {
-    return   <nav>
-    <div className="nav-wrapper">
-      <a href="#" classNameName="brand-logo">Logo</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
+  return (
+    <nav className="deep-purple accent-1">
+      <div className="nav-wrapper">
+        <a href="!#" className="brand-logo">
+          React Movies
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="!#">Repo</a>
+          </li>
+          <li>
+            <a href="badges.html">Components</a>
+          </li>
+          <li>
+            <a href="collapsible.html">JavaScript</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export { Header };
